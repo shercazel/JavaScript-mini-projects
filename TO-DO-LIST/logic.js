cba73c9cb4283dfd86087edaf3067fd0716122b9
sherwin Castillo <sherwincastillo454580@gmail.com>
2026-06-13 21:28:13 +0800

    learn sa basic ng Array

--- TO-DO-LIST/logic.js
+++ TO-DO-LIST/logic.js
@@ -1,34 +1,55 @@
 let addNewTask;
 let process;
 let running = true;
+let ask;
+// let i = 0
+
+
 let task = [];
 
 while (running) {
-    let choice = Number(prompt('Display\n1. Add Task \n2. View Tasks\n3. Exit\n  Enter your choice:'));
+    let choice = Number(prompt('Display\n1. Add Task \n2. View Tasks\n 3.Remove Task \n 4. Exit\n  Enter your choice:'));
     switch (choice) {
         case 1:
             while (running) {
-                let i = 1;
-                let ask = prompt("Press add for a new task, Press exit for exit")
+                ask = prompt("Press add for a new task, Press exit for exit")
                 if (ask == "add") {
                     addNewTask = prompt("PLease add a new task.");
                     task.push(addNewTask);
-                    console.log(`Task ${i++} + ${addNewTask}`)
+                    // console.log(`Task ${i++} ${addNewTask}`)
+                    console.log(`task ${task.length} ${addNewTask}`)
                 }
                 else if (ask == "exit") {
                     alert('you exit th add new task')
-                    running = false;
                     break;
                 }
                 else {
                     alert('invalid ka mag type boy, ayusin mo!')
                 }
             }
-
             break;
         case 2:
             break;
         case 3:
+            while (running) {
+                let found = true;
+                let allTask = "";
+                let choose;
+                for (let i = 0; i < task.length; i++) {
+                    allTask += `${i + 1} Your Task ${task[i]}\n`;
+                }
+                alert("Remove your Task? choose the right number base on the task list of your task\n" + allTask);
+
+
+
+
+
+
+
+            }
+
+            break;
+        case 4:
             alert('nag exit ka na sa system!')
             running = false;
             break;
